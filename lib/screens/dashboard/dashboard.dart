be4a0096e4c930a8dd85f6278afdce588c178bb3
WALEED AHMED <waleed.allshore@gmail.com>
2026-06-13 05:14:53 +0500

    Refactor dashboard sidebar into a dedicated `AppSidebar` widget and centralize navigation logic

--- lib/screens/dashboard/dashboard.dart
+++ lib/screens/dashboard/dashboard.dart
@@ -7,6 +7,7 @@ import 'package:skinsync_clinic_portal/screens/dashboard/inventory_screen.dart';
 import 'package:skinsync_clinic_portal/screens/dashboard/payment_and_wallet_screen.dart';
 import 'package:skinsync_clinic_portal/screens/dashboard/roles_screen.dart';
 import 'package:skinsync_clinic_portal/utils/responsive.dart';
+import 'package:skinsync_clinic_portal/widgets/app_sidebar.dart';
 import 'package:skinsync_clinic_portal/widgets/gradient_scaffold.dart';
 
 import '../../utils/assets.dart';
@@ -53,18 +54,18 @@ class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
     // Sync index with current route
-    final index = _getSelectedIndex(context);
-    if (_controller.selectedIndex != index) {
+    final index = AppSidebarRoutes.indexOf(GoRouterState.of(context).matchedLocation);
+    if (_controller.selectedIndex != index && index != -1) {
       _controller.selectIndex(index);
     }
 
     return GradientScaffold(
       // Use SidebarX as drawer on mobile/portrait
-      drawer: context.isLandscape ? null : _buildSidebar(context),
+      drawer: context.isLandscape ? null : AppSidebar(controller: _controller),
       body: Row(
         children: [
           // Permanent Sidebar on landscape (Desktop/Tablet)
-          if (context.isLandscape) _buildSidebar(context),
+          if (context.isLandscape) AppSidebar(controller: _controller),
           Expanded(
             child: Column(
               children: [
@@ -75,115 +76,6 @@ class _DashboardState extends State<Dashboard> {
           ),
         ],
       ),
-    );
-  }
-
-  int _getSelectedIndex(BuildContext context) {
-    final String location = GoRouterState.of(context).matchedLocation;
-    if (location.startsWith(HomeScreen.routeName)) return 0;
-    if (location.startsWith(PatientManagementScreen.routeName)) return 1;
-    if (location.startsWith(AppointmentScreen.routeName)) return 2;
-    if (location.startsWith(TreatmentScreen.routeName)) return 3;
-    if (location.startsWith(MangeDoctorsInjectorsScreen.routeName)) return 4;
-    if (location.startsWith(InventoryScreen.routeName)) return 5;
-    if (location.startsWith(RolesScreen.routeName)) return 6;
-    if (location.startsWith(ManageStaffScreen.routeName)) return 7;
-    if (location.startsWith(PaymentAndWalletScreen.routeName)) return 8;
-    if (location.startsWith(ProfileScreen.routeName)) return 9;
-    return 0;
-  }
-
-  Widget _buildSidebar(BuildContext context) {
-    return SidebarX(
-      controller: _controller,
-      theme: SidebarXTheme(
-        margin: EdgeInsets.all(context.w(10)),
-        padding: EdgeInsets.symmetric(vertical: context.h(20)),
-        decoration: BoxDecoration(
-          color: CustomColors.lightPurple2,
-          borderRadius: BorderRadius.circular(context.r(10)),
-        ),
-        hoverColor: CustomColors.purpleHover,
-        textStyle: context.fonts.grey14w500,
-        selectedTextStyle: context.fonts.black14w600,
-        hoverTextStyle: context.fonts.black14w600.copyWith(color: CustomColors.purple),
-        itemTextPadding: EdgeInsets.only(left: context.w(15)),
-        selectedItemTextPadding: EdgeInsets.only(left: context.w(15)),
-        itemMargin: EdgeInsets.symmetric(horizontal: context.w(10), vertical: context.h(4)),
-        selectedItemMargin: EdgeInsets.symmetric(horizontal: context.w(10), vertical: context.h(4)),
-        itemDecoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(context.r(12)),
-        ),
-        selectedItemDecoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(context.r(12)),
-          color: CustomColors.white,
-          boxShadow: [
-            BoxShadow(
-              color: CustomColors.black.withValues(alpha: 0.05),
-              blurRadius: context.r(10),
-              offset: Offset(0, context.h(2)),
-            ),
-          ],
-        ),
-        iconTheme: IconThemeData(
-          color: CustomColors.lightGrey,
-          size: context.r(20),
-        ),
-        selectedIconTheme: IconThemeData(
-          color: CustomColors.purple,
-          size: context.r(20),
-        ),
-      ),
-      extendedTheme: SidebarXTheme(
-        width: context.w(270),
-        decoration: BoxDecoration(
-          color: CustomColors.lightPurple2,
-          borderRadius: BorderRadius.circular(context.r(10)),
-        ),
-        margin: EdgeInsets.all(context.w(10)),
-      ),
-      headerBuilder: (context, extended) {
-        return Column(
-          children: [
-            SizedBox(height: context.h(20)),
-            Image.asset(
-              PngAssets.splashLogo,
-              width: context.r(48),
-              height: context.r(48),
-            ),
-            if (extended) ...[
-              SizedBox(height: context.h(10)),
-              Image.asset(PngAssets.logo, height: context.r(20)),
-            ],
-            SizedBox(height: context.h(30)),
-          ],
-        );
-      },
-      items: [
-        _buildSidebarItem(context, 'Home', Iconsax.home_2, HomeScreen.routeName),
-        _buildSidebarItem(context, 'Patient Management', Iconsax.profile_2user, PatientManagementScreen.routeName),
-        _buildSidebarItem(context, 'Appointments', Iconsax.calendar, AppointmentScreen.routeName),
-        _buildSidebarItem(context, 'Treatments', Icons.vaccines_outlined, TreatmentScreen.routeName),
-        _buildSidebarItem(context, 'Doctors / Injectors', Icons.masks_outlined, MangeDoctorsInjectorsScreen.routeName),
-        _buildSidebarItem(context, 'Inventory', Icons.inventory, InventoryScreen.routeName),
-        _buildSidebarItem(context, 'Roles', Icons.person_outline, RolesScreen.routeName),
-        _buildSidebarItem(context, 'Staff', Iconsax.user_octagon, ManageStaffScreen.routeName),
-        _buildSidebarItem(context, 'Payments & Wallets', Iconsax.wallet_3, PaymentAndWalletScreen.routeName),
-        _buildSidebarItem(context, 'Profile', Iconsax.profile_circle, ProfileScreen.routeName),
-      ],
-    );
-  }
-
-  SidebarXItem _buildSidebarItem(BuildContext context, String label, IconData icon, String route) {
-    return SidebarXItem(
-      icon: icon,
-      label: label,
-      onTap: () {
-        context.go(route);
-        if (Scaffold.of(context).isDrawerOpen) {
-          Navigator.pop(context);
-        }
-      },
     );
   }
 }
---
+++ lib/widgets/app_sidebar.dart
@@ -0,0 +1,208 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:sidebarx/sidebarx.dart';
+import 'package:skinsync_clinic_portal/screens/dashboard/appointment_screen.dart';
+import 'package:skinsync_clinic_portal/screens/dashboard/home_screen.dart';
+import 'package:skinsync_clinic_portal/screens/dashboard/inventory_screen.dart';
+import 'package:skinsync_clinic_portal/screens/dashboard/manage_doc_injector_screen.dart';
+import 'package:skinsync_clinic_portal/screens/dashboard/mange_staff_screen.dart';
+import 'package:skinsync_clinic_portal/screens/dashboard/patient_management.dart';
+import 'package:skinsync_clinic_portal/screens/dashboard/payment_and_wallet_screen.dart';
+import 'package:skinsync_clinic_portal/screens/dashboard/profile_screen.dart';
+import 'package:skinsync_clinic_portal/screens/dashboard/roles_screen.dart';
+import 'package:skinsync_clinic_portal/screens/dashboard/treatment_screen.dart';
+import '../utils/assets.dart';
+import '../utils/theme.dart';
+
+abstract final class AppSidebarRoutes {
+  static const routes = <String>[
+    HomeScreen.routeName,
+    PatientManagementScreen.routeName,
+    AppointmentScreen.routeName,
+    TreatmentScreen.routeName,
+    MangeDoctorsInjectorsScreen.routeName,
+    InventoryScreen.routeName,
+    RolesScreen.routeName,
+    ManageStaffScreen.routeName,
+    PaymentAndWalletScreen.routeName,
+    ProfileScreen.routeName,
+  ];
+
+  static int indexOf(String location) {
+    final exact = routes.indexOf(location);
+    if (exact >= 0) return exact;
+    for (var i = 0; i < routes.length; i++) {
+      if (location.startsWith(routes[i])) return i;
+    }
+    return -1;
+  }
+}
+
+class AppSidebar extends StatelessWidget {
+  const AppSidebar({
+    super.key,
+    required this.controller,
+    this.showToggleButton = true,
+  });
+
+  final SidebarXController controller;
+  final bool showToggleButton;
+
+  @override
+  Widget build(BuildContext context) {
+    return SidebarX(
+      controller: controller,
+      theme: _buildTheme(context),
+      extendedTheme: _buildExtendedTheme(context),
+      animationDuration: const Duration(milliseconds: 300),
+      showToggleButton: showToggleButton,
+      toggleButtonBuilder: (context, extended) => _buildToggleButton(context, extended),
+      headerDivider: const SizedBox.shrink(),
+      footerDivider: Divider(color: CustomColors.border, height: context.h(1), thickness: 1),
+      separatorBuilder: (context, index) => _separatorBuilder(context, index, controller),
+      headerBuilder: (context, extended) => _headerBuilder(context, extended),
+      items: _buildItems(context),
+    );
+  }
+
+  SidebarXTheme _buildTheme(BuildContext context) {
+    return SidebarXTheme(
+      width: context.w(80),
+      decoration: const BoxDecoration(
+        color: CustomColors.white,
+        border: Border(right: BorderSide(color: CustomColors.border, width: 1)),
+      ),
+      padding: context.appEdgeInsets(vertical: 24),
+      iconTheme: IconThemeData(color: CustomColors.grey, size: context.sp(20)),
+      selectedIconTheme: IconThemeData(color: CustomColors.purple, size: context.sp(20)),
+      hoverIconTheme: IconThemeData(color: CustomColors.purple, size: context.sp(20)),
+      textStyle: context.fonts.grey14w600,
+      selectedTextStyle: context.fonts.purple14w600,
+      hoverTextStyle: context.fonts.purple14w600,
+      hoverColor: CustomColors.lightPurple,
+      itemMargin: context.appEdgeInsets(horizontal: 12, vertical: 4),
+      selectedItemMargin: context.appEdgeInsets(horizontal: 12, vertical: 4),
+      itemPadding: context.appEdgeInsets(horizontal: 12, vertical: 12),
+      selectedItemPadding: context.appEdgeInsets(horizontal: 12, vertical: 12),
+      itemDecoration: BoxDecoration(borderRadius: context.borderRadius(all: 12)),
+      selectedItemDecoration: BoxDecoration(
+        color: CustomColors.lightPurple,
+        borderRadius: context.borderRadius(all: 12),
+        border: Border.all(color: CustomColors.purple.withValues(alpha: 0.2), width: 1),
+      ),
+    );
+  }
+
+  SidebarXTheme _buildExtendedTheme(BuildContext context) {
+    return _buildTheme(context).copyWith(
+      width: context.w(280),
+      itemTextPadding: context.appEdgeInsets(left: 16),
+      selectedItemTextPadding: context.appEdgeInsets(left: 16),
+    );
+  }
+
+  List<SidebarXItem> _buildItems(BuildContext context) {
+    return [
+      SidebarXItem(icon: Iconsax.home_2, label: 'Home', onTap: () => _onItemTap(context, 0)),
+      SidebarXItem(icon: Iconsax.profile_2user, label: 'Patient Management', onTap: () => _onItemTap(context, 1)),
+      SidebarXItem(icon: Iconsax.calendar, label: 'Appointments', onTap: () => _onItemTap(context, 2)),
+      SidebarXItem(icon: Icons.vaccines_outlined, label: 'Treatments', onTap: () => _onItemTap(context, 3)),
+      SidebarXItem(icon: Icons.masks_outlined, label: 'Doctors / Injectors', onTap: () => _onItemTap(context, 4)),
+      SidebarXItem(icon: Icons.inventory, label: 'Inventory', onTap: () => _onItemTap(context, 5)),
+      SidebarXItem(icon: Icons.person_outline, label: 'Roles', onTap: () => _onItemTap(context, 6)),
+      SidebarXItem(icon: Iconsax.user_octagon, label: 'Staff', onTap: () => _onItemTap(context, 7)),
+      SidebarXItem(icon: Iconsax.wallet_3, label: 'Payments & Wallets', onTap: () => _onItemTap(context, 8)),
+      SidebarXItem(icon: Iconsax.profile_circle, label: 'Profile', onTap: () => _onItemTap(context, 9)),
+    ];
+  }
+
+  void _onItemTap(BuildContext context, int index) {
+    context.go(AppSidebarRoutes.routes[index]);
+    if (Scaffold.of(context).isDrawerOpen) {
+      Navigator.pop(context);
+    }
+  }
+
+  Widget _separatorBuilder(BuildContext context, int index, SidebarXController controller) {
+    if (index == 2) return _SectionLabel(title: 'CLINICAL', controller: controller);
+    if (index == 4) return _SectionLabel(title: 'OPERATIONS', controller: controller);
+    if (index == 7) return _SectionLabel(title: 'FINANCIALS', controller: controller);
+    if (index == 8) return _SectionLabel(title: 'SYSTEM', controller: controller);
+    return context.verticalSpace(2);
+  }
+
+  Widget _headerBuilder(BuildContext context, bool extended) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 300),
+      padding: context.appEdgeInsets(
+        vertical: 28,
+        horizontal: extended ? 24 : 12,
+      ),
+      child: Row(
+        mainAxisAlignment: extended ? MainAxisAlignment.start : MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            width: context.w(extended ? 42 : 30),
+            height: context.w(extended ? 42 : 30),
+            child: Image.asset(PngAssets.splashLogo, fit: BoxFit.contain),
+          ),
+          if (extended) ...[
+            context.horizontalSpace(14),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text('SkinSync', style: context.fonts.black18w600lsNeg04),
+                  Text('CLINIC PORTAL', style: context.fonts.purple9w800ls1),
+                ],
+              ),
+            ),
+          ],
+        ],
+      ),
+    );
+  }
+
+  Widget _buildToggleButton(BuildContext context, bool extended) {
+    return InkWell(
+      onTap: () => controller.toggleExtended(),
+      hoverColor: CustomColors.purple.withValues(alpha: 0.05),
+      child: Container(
+        width: double.infinity,
+        padding: context.appEdgeInsets(vertical: 16),
+        child: Icon(
+          extended ? Icons.arrow_back_ios_new_rounded : Icons.arrow_forward_ios_rounded,
+          size: context.sp(16),
+          color: CustomColors.grey.withValues(alpha: 0.7),
+        ),
+      ),
+    );
+  }
+}
+
+class _SectionLabel extends StatelessWidget {
+  final String title;
+  final SidebarXController controller;
+  const _SectionLabel({required this.title, required this.controller});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: controller,
+      builder: (context, child) {
+        if (!controller.extended) {
+          return Padding(
+            padding: context.appEdgeInsets(vertical: 8),
+            child: Divider(color: CustomColors.border, indent: context.w(20), endIndent: context.w(20)),
+          );
+        }
+        return Padding(
+          padding: context.appEdgeInsets(left: 28, top: 24, right: 16, bottom: 8),
+          child: Text(title, style: context.fonts.grey11w600ls12),
+        );
+      },
+    );
+  }
+}
