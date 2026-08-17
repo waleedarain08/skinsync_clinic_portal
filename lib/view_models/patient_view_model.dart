9875b0524d305fa64e98f87d0f86594b88628f46
WALEED AHMED <waleed.allshore@gmail.com>
2026-08-17 16:09:02 +0500

    feat: update treatmentRequests assignment in patient view model for accurate data handling

--- lib/view_models/patient_view_model.dart
+++ lib/view_models/patient_view_model.dart
@@ -133,9 +133,7 @@ class PatientViewModel extends BaseViewModel<PatientState> {
       if (response.success) {
         state = state.copyWith(
           treatmentLoading: false,
-          treatmentRequests: (response.data ?? [])
-              .where((element) => element.userId == patientId)
-              .toList(),
+          treatmentRequests: response.data ?? [],
           treatmentTotalPage: response.totalPages,
           treatmentTotalResults: response.total,
           treatmentPage: response.page,
