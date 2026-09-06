18bb77016422784de17042c5b204870551a879b7
saboor289 <syedsuboorhaiderjillani@gmail.com>
2026-09-06 22:55:44 +0500

    Revert "refactor: clean up main function and improve TodaysCheckInTile layout and styling"
    This reverts commit c6427906e7016c7c8ea04351bdefbee3e50c6896.

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ bool isDeploymentMode = false;
 
 
 Future<void> main() async {
+
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await ScreenUtilPlus.ensureScreenSize();
