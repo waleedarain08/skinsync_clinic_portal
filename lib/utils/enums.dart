bb6d40d7a55678f13a7557e3f6fff7d6597c9100
tauqeerkhattak <tauqeer745@outlook.com>
2026-09-10 20:44:10 +0500

    feat: update QA API base URL to development server in BaseUrls enum

--- lib/utils/enums.dart
+++ lib/utils/enums.dart
@@ -106,9 +106,9 @@ enum Endpoint {
 
 enum BaseUrls {
   api('https://api.skinsyncai.com/api/'),
-  // apiQa('https://api-dev.skinsyncai.com/api/');
+  apiQa('https://api-dev.skinsyncai.com/api/');
 
-  apiQa('http://localhost:8084/api/');
+  // apiQa('http://localhost:8084/api/');
 
   final String url;
 
