0857ecb3239bddb8a6accf27b3836fa6861218ba
WALEED AHMED <waleed.allshore@gmail.com>
2026-09-14 21:41:24 +0500

    refactor: extract web iframe registration to conditional helper and add non-web fallback in about screen

---
+++ lib/screens/about_screen_stub.dart
@@ -0,0 +1,10 @@
+void registerIframeViews({
+  required String termsUrl,
+  required String privacyUrl,
+  required String termsViewType,
+  required String privacyViewType,
+  required void Function() onTermsLoaded,
+  required void Function() onPrivacyLoaded,
+}) {
+  // Stub for non-web platforms
+}
